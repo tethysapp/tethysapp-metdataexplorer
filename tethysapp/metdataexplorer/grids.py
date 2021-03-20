62a9b790ae25df9058171554329a73ec43e90fd2
jenochjones <jon.enoch.jones@gmail.com>
2021-03-20 17:48:24 -0600

    Updated grids

--- tethysapp/metdataexplorer/grids.py
+++ tethysapp/metdataexplorer/grids.py
@@ -3,12 +3,12 @@ import grids
 import tempfile
 import os
 import json
-import urllib
+# import urllib
 import requests
-import pandas
-import math
+# import pandas
+# import math
 import geopandas as gpd
-import netCDF4 as nc
+# import netCDF4 as nc
 from geojson import dump
 
 from .timestamp import iterate_files
@@ -35,22 +35,23 @@ def organize_array(attribute_array):
         variables += 'var=' + variable + '&'
 
     epsg = attribute_array['epsg']
-    files, geojson_geometry, geojson_path = get_geojson_and_data(access_urls['NetcdfSubset'], attribute_array['spatial'], variables, epsg)
+    geojson_geometry, geojson_path = get_geojson_and_data(attribute_array['spatial'], epsg) #variables, access_urls['NetcdfSubset'],
+    #files,
 
     data = {}
     for variable in attribute_array['attributes']:
         dims = attribute_array['attributes'][variable]['dimensions'].split(',')
         dim_order = (dims[0], dims[1], dims[2])
         stats_value = 'mean'
-        timeseries = get_timeseries_at_geojson([files], variable, dim_order, geojson_geometry, geojson_path, stats_value)
+        timeseries = get_timeseries_at_geojson([access_urls['OPENDAP']], variable, dim_order, geojson_geometry)
         data[variable] = timeseries
 
     os.remove(geojson_path)
-    os.remove(files)
+#    os.remove(files)
     return data
 
 
-def get_geojson_and_data(netcdf_subset_url, spatial, var, epsg):
+def get_geojson_and_data(spatial, epsg): #netcdf_subset_url,var,
     print(spatial)
     print(type(spatial))
     geojson_path = os.path.join(tempfile.gettempdir(), 'temp.json')
@@ -61,7 +62,6 @@ def get_geojson_and_data(netcdf_subset_url, spatial, var, epsg):
             dump(spatial, f)
         geojson_geometry = gpd.read_file(data)
         os.remove(data)
-        print(geojson_geometry)
     elif spatial[:4] == 'http':
         data = requests.Request('GET', spatial).url
         geojson_geometry = gpd.read_file(data)
@@ -69,46 +69,52 @@ def get_geojson_and_data(netcdf_subset_url, spatial, var, epsg):
         data = os.path.join(os.path.dirname(__file__), 'workspaces', 'app_workspace', spatial + '.geojson')
         geojson_geometry = gpd.read_file(data)
 
-    west = math.floor(min(geojson_geometry['geometry'].bounds['minx']) - 1)
-    south = math.floor(min(geojson_geometry['geometry'].bounds['miny']) - 1)
-    east = math.ceil(max(geojson_geometry['geometry'].bounds['maxx']) + 1)
-    north = math.ceil(max(geojson_geometry['geometry'].bounds['maxy']) + 1)
-    subset_url = netcdf_subset_url + '?' + var + 'north=' + str(north) + '&west=' + str(west) + '&east=' + str(east) + '&south=' + str(south) + '&disableProjSubset=on&horizStride=1&temporal=all'
-    path_to_netcdf = os.path.join(tempfile.gettempdir(), 'temp.nc')
-    urllib.request.urlretrieve(subset_url, path_to_netcdf)
+#    west = math.floor(min(geojson_geometry['geometry'].bounds['minx']) - 1)
+#    south = math.floor(min(geojson_geometry['geometry'].bounds['miny']) - 1)
+#    east = math.ceil(max(geojson_geometry['geometry'].bounds['maxx']) + 1)
+#    north = math.ceil(max(geojson_geometry['geometry'].bounds['maxy']) + 1)
+#    subset_url = netcdf_subset_url + '?' + var + 'north=' + str(north) + '&west=' + str(west) + '&east=' + str(east) + '&south=' + str(south) + '&disableProjSubset=on&horizStride=1&temporal=all'
+#    path_to_netcdf = os.path.join(tempfile.gettempdir(), 'temp.nc')
+#    urllib.request.urlretrieve(subset_url, path_to_netcdf)
 
-    if len(epsg) > 4 and not str(epsg) == 'false':
-        shift_lat = int(epsg.split(',')[2][2:])
-        shift_lon = int(epsg.split(',')[1][2:])
-        geojson_geometry['geometry'] = geojson_geometry.translate(xoff=shift_lon, yoff=shift_lat)
+    if not str(epsg) == 'false':
+        print(str(geojson_geometry.crs)[5:])
+        if not str(epsg)[:4] == str(geojson_geometry.crs)[5:]:
+            geojson_geometry = geojson_geometry.to_crs('EPSG:' + str(epsg)[:4])
+        if len(epsg) > 4:
+            shift_lat = int(epsg.split(',')[2][2:])
+            shift_lon = int(epsg.split(',')[1][2:])
+            geojson_geometry['geometry'] = geojson_geometry.translate(xoff=shift_lon, yoff=shift_lat)
 
-    return path_to_netcdf, geojson_geometry, geojson_path
+    return geojson_geometry, geojson_path #path_to_netcdf,
 
 
-def get_timeseries_at_geojson(files, var, dim_order, geojson_geometry, geojson_path, stats_value):
+def get_timeseries_at_geojson(files, var, dim_order, geojson_geometry): # geojson_path, stats_value):
     series = grids.TimeSeries(files=files, var=var, dim_order=dim_order)
-    #series.interp_units = True
-    data_frame = pandas.DataFrame()
-    data_frame.name = var
-    for index, row in geojson_geometry.iterrows():
-        print(row.id)
-        new_geom = gpd.GeoSeries(row.geometry)
-        new_geom.to_file(geojson_path, driver="GeoJSON")
-        timeseries = series.shape(geojson_path, )
-        if index == 0:
-            data_frame['datetime'] = timeseries['datetime']
-            data_frame[row.id] = timeseries[stats_value]
-        else:
-            data_frame[row.id] = timeseries[stats_value]
+    timeseries_array = series.shape(vector=geojson_geometry, behavior='features', labelby='id')
+    print(timeseries_array)
+    # series.interp_units = True
+    # data_frame = pandas.DataFrame()
+    # data_frame.name = var
+    # for index, row in geojson_geometry.iterrows():
+    #    print(row.id)
+    #    new_geom = gpd.GeoSeries(row.geometry)
+    #    new_geom.to_file(geojson_path, driver="GeoJSON")
+    #    timeseries = series.shape(geojson_path, )
+    #    if index == 0:
+    #        data_frame['datetime'] = timeseries['datetime']
+    #        data_frame[row.id] = timeseries[stats_value]
+    #    else:
+    #        data_frame[row.id] = timeseries[stats_value]
 
-    ################Remove when grids formats times#################
-    netcdf = nc.Dataset(files[0])
-    units = netcdf[dim_order[0]].units
-    times = netcdf[dim_order[0]][:]
-    time_list = []
-    for ti in times:
-        t = nc.num2date(ti, units)
-        time_list.append(str(t))
-    data_frame['datetime'] = time_list
+    # ###############Remove when grids formats times#################
+    # netcdf = nc.Dataset(files[0])
+    # units = netcdf[dim_order[0]].units
+    # times = netcdf[dim_order[0]][:]
+    # time_list = []
+    # for ti in times:
+    #    t = nc.num2date(ti, units)
+    #    time_list.append(str(t))
+    # data_frame['datetime'] = time_list
     ################################################################
-    return data_frame
+    return timeseries_array  # data_frame
